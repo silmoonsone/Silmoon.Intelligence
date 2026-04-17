98f02e722e46d9c4d60f583b91752710fb28c5d0
WEIBIN SONG <sone@silmoon.com>
2026-04-18 05:30:58 +0800

    添加项目文件。

---
+++ Silmoon.Intelligence.MauiClient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Silmoon.Intelligence.MauiClient
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Silmoon.Intelligence.WinUIClient/Pages/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Silmoon.Intelligence.WinUIClient.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MainPageViewModel ViewModel { get; } = new MainPageViewModel();
+        public MainPage()
+        {
+            DataContext = ViewModel;
+            InitializeComponent();
+        }
+    }
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public partial string Url { get; set; } = "https://www.silmoon.com/";
+        [RelayCommand]
+        public void NewWindow()
+        {
+            var MainWindow = new MainWindow();
+            MainWindow.ExtendsContentIntoTitleBar = true;
+            MainWindow.ctlMainPageFrame.Navigate(typeof(MainPage));
+            MainWindow.Activate();
+        }
+    }
+
+}
