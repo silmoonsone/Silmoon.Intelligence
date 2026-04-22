2eed0cc9d823cbb57c63056ff6334a1ee776108b
WEIBIN SONG <sone@silmoon.com>
2026-04-22 14:33:35 +0800

    add WinForm testing.

---
+++ Silmoon.Intelligence.WinFormTesting/Form1.cs
@@ -0,0 +1,10 @@
+namespace Silmoon.Intelligence.WinFormTesting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
